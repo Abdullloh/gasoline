b4da8619cca73415b47023a3f9b49423a0da3bcd
abduvaxobovnodirbek <nodirbekabduvaxobov17@gmail.com>
2022-06-23 15:43:11 +0500

    ..

---
+++ src/views/UserAccount/Purchases/Purchases.js
@@ -0,0 +1,188 @@
+import React, { useState, useEffect } from "react";
+import { Button, Checkbox, Popconfirm, Table } from "antd";
+import { FiPlus } from "react-icons/fi";
+import { AiOutlineSearch, AiOutlineDelete } from "react-icons/ai";
+import { StyledPurchases } from "./Purchases.style";
+import OilImg from "../../../assets/img/oil-img.svg";
+import { Link, useNavigate } from "react-router-dom";
+import Axios from "../../../utils/axios";
+import EditIcon from "../../../assets/img/edit-alt.svg";
+
+function Purchases() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  let adminInfo = JSON.parse(localStorage.getItem("user_info"))?.data;
+  let header = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${adminInfo?.token?.access}`,
+  };
+
+  const getProducts = async () => {
+    setLoading(true);
+    try {
+      const res = await Axios.get("/adminside/products/", { headers: header });
+      setData(res.data.results);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  const getById = (id) => {
+    navigate(`/purchases/${id}`);
+  };
+  const handleProductSale = async (id, status) => {
+    setLoading(true);
+    try {
+      const res = await Axios.patch(`/adminside/product/${id}`, {
+        id,
+        available: status,
+      });
+      if (res?.status == 200) {
+        getProducts();
+      }
+      console.log(res);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const columns = [
+    {
+      dataIndex: "productName",
+      render: (text, record) => (
+        <td className="ant-table-cell">
+          <div className="img_column">
+            {record?.images[0]?.image ? (
+              <img
+                className="product_img"
+                src={record?.images[0].image}
+                alt="Product"
+              />
+            ) : (
+              <img className="product_img" src={OilImg} alt="Product" />
+            )}
+            <div>
+              <h3 className="product_name">{record.title}</h3>
+              <Checkbox
+                checked={record?.available}
+                size="small"
+                onClick={() =>
+                  handleProductSale(record?.id, !record?.available)
+                }
+              >
+                В наличи
+              </Checkbox>
+            </div>
+          </div>
+        </td>
+      ),
+    },
+    {
+      dataIndex: "price",
+      render: (text) => (
+        <td className="ant-table-cell">
+          <h3 className="product_price">{text}</h3>
+        </td>
+      ),
+    },
+    {
+      dataIndex: "id",
+      render: (text, record) => (
+        <td className="ant-table-cell">
+          <div className="edit_column">
+            <div onClick={() => getById(text)}>
+              <img src={EditIcon} alt="edit-icon" />
+            </div>
+            <div>
+              <Popconfirm
+                title="Are you sure？"
+                okText="Yes"
+                cancelText="No"
+                onConfirm={() => handleDelete(record.id)}
+              >
+                <AiOutlineDelete
+                  className="edit_column"
+                  color="red"
+                  size="20"
+                />
+              </Popconfirm>
+            </div>
+          </div>
+        </td>
+      ),
+    },
+  ];
+  const handleDelete = async (id) => {
+    try {
+      const res = await Axios.delete(`/adminside/product/${id}`);
+      console.log(res);
+      if (res.status == 204) {
+        let filterData = data.filter((item) => item.id !== id);
+        setData(filterData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      let searchedData = data.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(searchedData);
+    } else {
+      setFilteredResults(data);
+    }
+  };
+
+  return (
+    <StyledPurchases>
+      <header>
+        <h2 className="title">Товары</h2>
+        <Link to="/add-product">
+          <Button type="primary" size="large  ">
+            <FiPlus color="#fff" size="16" />
+            Добавить новый товар
+          </Button>
+        </Link>
+        <div className="search_block">
+          <input
+            type="text"
+            placeholder="Название товара,артикул,уникальный код"
+            value={searchInput}
+            onChange={(e) => searchItems(e.target.value)}
+          />
+          <AiOutlineSearch color="#000" size="24" />
+        </div>
+      </header>
+      <div className="wrapper">
+        {searchInput.length > 1 ? (
+          <Table
+            dataSource={filteredResults}
+            columns={columns}
+            loading={loading}
+          />
+        ) : (
+          <Table dataSource={data} columns={columns} loading={loading} />
+        )}
+      </div>
+    </StyledPurchases>
+  );
+}
+
+export default Purchases;
--- src/views/UserAccount/UserAccount.jsx
+++ src/views/UserAccount/UserAccount.jsx
@@ -7,7 +7,8 @@ import { UserAccountWrapper } from "./useAccounStyle";
 import {useDispatch,useSelector} from "react-redux"
 import { getUserOrders, getUserInfo, editPartnerInfo } from "../../Redux/userInfos/user";
 import exit from "../../assets/img/exit.svg"
-import AddProduct from "../Admin/containers/AddProduct/Addproduct"
+import AddProduct from "./AddProduct/Addproduct"
+import Purchases from "./Purchases/Purchases";
 
 export default function UserAccount() {
   const navigate = useNavigate()
@@ -16,6 +17,7 @@ export default function UserAccount() {
   const [orderSection,setOrderSection] = useState(false)
   //belong to partner
   const [edit_partnerInfo,set_edit_partnerInfo] = useState(false)
+  const [viewPurchase,setViewPurchase] = useState(false)
 
   
   const userInfo = useSelector(state=>state.user.userInfo)
@@ -42,19 +44,28 @@ export default function UserAccount() {
 // universal
 const toUserDetail = ()=>{
   setOrderSection(false)
+  setViewPurchase(false)
   dispatch(getUserInfo())
 }
 
 // belong to customer
   const toOrdersSection = ()=>{
      setOrderSection(true)
+    setViewPurchase(false)
      dispatch(getUserOrders())
   }
 
 //belong to partner
  const handlePartnerInfo = ()=>{
     set_edit_partnerInfo(true)
+    setViewPurchase(false)
  }
+
+ //belong to partner
+ const handleViewPurchase = ()=>{
+    set_edit_partnerInfo(true)
+    setViewPurchase(true)
+}
 
 
  let initialValues = {
@@ -162,7 +173,7 @@ const handleSubmit = async (data,{resetForm})=>{
       <div className="left-side">
          <ul>
            <li onClick = {()=>navigate("/")}>Bosh sahifa</li>
-           <li onClick = {()=>navigate("/")}>Tavarlar</li>
+           <li onClick = {handleViewPurchase}>Tavarlar</li>
            <li onClick = {()=>set_edit_partnerInfo(false)}>Shaxsiy malumot</li>
            <li>Kompaniya</li>
          <h4  onClick={()=> logout()}> <img src={exit} alt="exit"/>  Выйти</h4>
@@ -216,82 +227,83 @@ const handleSubmit = async (data,{resetForm})=>{
         <div>
           <button onClick = {handlePartnerInfo}>Изменить</button>
         </div>
-
-        <AddProduct/>
+        
+        
 
       </>:<>
-      <h2>Изменение данных*</h2>
+       {
+        viewPurchase ? <Purchases/>:
+        <>
+         <h2>Изменение данных*</h2>
 
-      <Formik
-           initialValues = {initialValues}
-           validationSchema = {validationSchema}
-           onSubmit = {handleSubmit}
-           enableReinitialize
-      >
-         {
-         formik=>{
-           return(
-            <Form>
-              <div className = "flex-item">
-              <label className = "item" htmlFor = "ceos_name">Полное наименование</label> 
-              <Field type="text"  id = "ceos_name"  name = "ceos_name"/> 
-              </div> 
+<Formik
+     initialValues = {initialValues}
+     validationSchema = {validationSchema}
+     onSubmit = {handleSubmit}
+     enableReinitialize
+>
+   {
+   formik=>{
+     return(
+      <Form>
+        <div className = "flex-item">
+        <label className = "item" htmlFor = "ceos_name">Полное наименование</label> 
+        <Field type="text"  id = "ceos_name"  name = "ceos_name"/> 
+        </div> 
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor = "name">Генеральный директор</label>
-         <Field type="text"  id = "name"  name = "name"/> 
-         </div> 
+   <div className = "flex-item">
+   <label className = "item" htmlFor = "name">Генеральный директор</label>
+   <Field type="text"  id = "name"  name = "name"/> 
+   </div> 
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor = "bank_name">Наименование банка</label>
-         <Field type="text"  id = "bank_name"  name = "bank_name"/> 
-         </div> 
-         
+   <div className = "flex-item">
+   <label className = "item" htmlFor = "bank_name">Наименование банка</label>
+   <Field type="text"  id = "bank_name"  name = "bank_name"/> 
+   </div> 
+   
 
-         <div className = "flex-item">
-        <label className = "item" htmlFor = "phone">Номер телефона</label> 
-        <Field type="number"  id = "phone"  name = "phone"/> 
-         </div> 
-         
+   <div className = "flex-item">
+  <label className = "item" htmlFor = "phone">Номер телефона</label> 
+  <Field type="number"  id = "phone"  name = "phone"/> 
+   </div> 
+   
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor = "inn">ИНН</label>   
-         <Field type="string"  id = "inn"  name = "inn"/> 
-         </div> 
+   <div className = "flex-item">
+   <label className = "item" htmlFor = "inn">ИНН</label>   
+   <Field type="string"  id = "inn"  name = "inn"/> 
+   </div> 
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor ="mfo">МФО</label>
-         <Field type="string"  id = "mfo"  name = "mfo"/> 
-         </div> 
+   <div className = "flex-item">
+   <label className = "item" htmlFor ="mfo">МФО</label>
+   <Field type="string"  id = "mfo"  name = "mfo"/> 
+   </div> 
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor = "company_address">Адрес компании</label>
-         <Field type="string"  id = "company_address"  name = "company_address"/> 
-         </div> 
+   <div className = "flex-item">
+   <label className = "item" htmlFor = "company_address">Адрес компании</label>
+   <Field type="string"  id = "company_address"  name = "company_address"/> 
+   </div> 
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor = "bank_account">Расчетный счет</label>
-         <Field type="string"  id = "bank_account"  name = "bank_account"/> 
-         </div> 
+   <div className = "flex-item">
+   <label className = "item" htmlFor = "bank_account">Расчетный счет</label>
+   <Field type="string"  id = "bank_account"  name = "bank_account"/> 
+   </div> 
 
-         <div className = "flex-item">
-         <label className = "item" htmlFor = "email">Логин</label> 
-         <Field type="email"  id = "email"  name = "email"/> 
-         </div>
+   <div className = "flex-item">
+   <label className = "item" htmlFor = "email">Логин</label> 
+   <Field type="email"  id = "email"  name = "email"/> 
+   </div>
 
-         <div>
-          <button disabled={formik.isSubmitting || !formik.isValid } type = "submit">Сохранить</button>
-        </div>
+   <div>
+    <button disabled={formik.isSubmitting || !formik.isValid } type = "submit">Сохранить</button>
+  </div>
 
-            </Form>
-            )} 
-        }   
-      </Formik>  
+      </Form>
+      )} 
+  }   
+</Formik>  
 
-
-
- 
-
+        </>
+       }
       </>
       }
       </div>
