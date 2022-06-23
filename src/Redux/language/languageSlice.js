cb65b993120a63f3d9d4615a582add9cd2d15528
abduvaxobovnodirbek <nodirbekabduvaxobov17@gmail.com>
2022-06-23 15:03:39 +0500

    ..

--- src/Redux/language/languageSlice.js
+++ src/Redux/language/languageSlice.js
@@ -1,5 +1,27 @@
-import { data } from "../data/data";
 const { createSlice } = require("@reduxjs/toolkit");
+
+
+// import { loginData } from "";
+// import { SidebarData } from "";
+// import {othersData  } from "";
+
+const data = {
+    uz: {
+    //   loginData: loginData.uz,
+    //   sidebar: SidebarData.uz,
+    //   others: othersData.uz,
+    },
+    ru: {
+    //   loginData: loginData.ru,
+    //   sidebar: SidebarData.ru,
+    //   others: othersData.ru,
+    },
+    en: {
+    //   loginData: loginData.en,
+    //   sidebar: SidebarData.en,
+    },
+  };
+
 
 const initialState = {
   name: "language",
@@ -19,3 +41,4 @@ const languageSlice = createSlice({
 export default languageSlice.reducer;
 
 export const { setLanguage } = languageSlice.actions;
+
